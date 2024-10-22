328cfe624d7a0a0f60f725c3425aaa2cdb5addc2
Tami <paeztami09@gmail.com>
2024-10-22 20:30:42 -0300

    'Get creado'

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ def crear_mensaje(mensaje : Mensaje):
     mensaje.id = len(mensajes_db) +1
     mensajes_db.append(mensaje)
     return mensaje
+
+@web.get("/mensajes/{mensaje_id}", response_model=Mensaje)
+def obtener_mensaje(mensaje_id: int):
+    for mensaje in mensajes_db:
+        if mensaje.id == mensaje_id:
+            return mensaje
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
